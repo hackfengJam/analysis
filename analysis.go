8c9b90f31c48b815dc4cfcab405f635c5f9f6eae
HackFun <hackfeng624@gmail.com>
2019-05-15 23:30:14 +0800

    完成流量统计系统

--- analysis.go
+++ analysis.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"bufio"
+	"crypto/md5"
+	"encoding/hex"
 	"flag"
+	"github.com/mediocregopher/radix.v2/pool"
+	"github.com/mgutz/str"
 	"github.com/sirupsen/logrus"
 	"io"
+	"net/url"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
+
+const HANDLE_DIG = " /dig?"
+const HANDLE_MOVIE = "/movie/"
+const HANDLE_LIST = "/list/"
+const HANDLE_HTML = ".html"
 
 type cmdParams struct {
 	logFilePath string
@@ -20,11 +32,15 @@ type digData struct {
 	ua    string
 }
 type urlData struct {
-	data digData
-	uid  string
+	data  digData
+	uid   string
+	unode urlNode
 }
 type urlNode struct {
-	//
+	unType string // 详情页 或者 列表页 或者 首页
+	unRid  int    // Resource ID  资源ID
+	unUrl  string // 当前页面的 url
+	unTime string // 当前访问页面的时间
 }
 type storageBlock struct {
 	counterType  string
@@ -33,10 +49,17 @@ type storageBlock struct {
 }
 
 var log = logrus.New()
+//var redisCli redis.Client
 
 func init() {
 	log.Out = os.Stdout
 	log.SetLevel(logrus.DebugLevel)
+	/*	redisCli, err := redis.Dial("tcp", "localhost:6379")
+		if err != nil {
+			log.Fatalln("Redis connect failed")
+		} else {
+			defer redisCli.Close()
+		}*/
 }
 
 func main() {
@@ -62,6 +85,20 @@ func main() {
 	var uvChannel = make(chan urlData, params.routineNum)
 	var storageChannel = make(chan storageBlock, params.routineNum)
 
+	// Redis Pool
+	redisPool, err := pool.New("tcp", "localhost:6379", 2*params.routineNum)
+	if err != nil {
+		log.Fatalln("Redis pool created failed.")
+		panic(err)
+	} else {
+		go func() {
+			for {
+				redisPool.Cmd("PING")
+				time.Sleep(3 * time.Second)
+			}
+		}()
+	}
+
 	// 日志消费者
 	go readFileLineByLine(params, logChannel)
 
@@ -72,28 +109,117 @@ func main() {
 
 	// 创建PV UV 统计器
 	go pvCounter(pvChannel, storageChannel)
-	go uvCounter(uvChannel, storageChannel)
+	go uvCounter(uvChannel, storageChannel, redisPool)
 	// 可扩展的 xxxCounter
 
 	// 创建存储器
-	go dataStorage(storageChannel)
+	go dataStorage(storageChannel, redisPool)
 
 	time.Sleep(1000 * time.Second)
 }
 
-func dataStorage(storageChannel chan storageBlock) {
+// HBase 列式存储，劣势：列簇 需要声明清楚（列变来变去不方便）
+func dataStorage(storageChannel chan storageBlock, redisPool *pool.Pool) {
+	for block := range storageChannel {
+		prefix := block.counterType + "_"
 
+		// 10000 * N * M
+		// 逐层添加，加洋葱皮的过程
+		// 维度： 天-小时-分钟
+		// 层级： 定级-大分类-小分类-终极页面
+		// 存储模型： Redis SortedSet
+		setKeys := []string{
+			prefix + "day_" + getTime(block.unode.unTime, "day"),
+			prefix + "hour_" + getTime(block.unode.unTime, "hour"),
+			prefix + "min_" + getTime(block.unode.unTime, "min"),
+			prefix + block.unode.unType + "_day_" + getTime(block.unode.unTime, "day"),
+			prefix + block.unode.unType + "_hour_" + getTime(block.unode.unTime, "hour"),
+			prefix + block.unode.unType + "_min_" + getTime(block.unode.unTime, "min"),
+		}
+
+		rowId := block.unode.unRid
+
+		for _, key := range setKeys {
+			ret, err := redisPool.Cmd(block.storageModel, key, 1, rowId).Int()
+			if err != nil || ret <= 0 {
+				log.Error("DataStorage redis storage error.", block.storageModel, key, rowId)
+			}
+		}
+	}
 }
 
-func uvCounter(uvChannel chan urlData, storageChannel chan storageBlock) {
-
+func uvCounter(uvChannel chan urlData, storageChannel chan storageBlock, redisPool *pool.Pool) {
+	for data := range uvChannel {
+		// HyperLoglog redis 去重
+		hyperLogLogKey := "uv_hpll_" + getTime(data.data.time, "day")
+		ret, err := redisPool.Cmd("PFADD", hyperLogLogKey, data.uid, "EX", 86400).Int()
+		if err != nil {
+			log.Warning("uvCounter check redis hyperloglog failed, ", err)
+		}
+		if ret != 1 {
+			continue
+		}
+		sItem := storageBlock{"uv", "ZINCRBY", data.unode}
+		storageChannel <- sItem
+	}
 }
 
 func pvCounter(pvChannel chan urlData, storageChannel chan storageBlock) {
-
+	for data := range pvChannel {
+		sItem := storageBlock{"pv", "ZINCRBY", data.unode}
+		storageChannel <- sItem
+	}
 }
 
-func logConsumer(logChannel chan string, pvChannel, uvChannel chan urlData) {
+func logConsumer(logChannel chan string, pvChannel, uvChannel chan urlData) error {
+	for logStr := range logChannel {
+		// 切割日志字符串，扣出打点上报的数据
+		data := cutLogFetchData(logStr)
+
+		// uid
+		// 说明： 课程中模拟生成uid,   md5(refer+ua)
+		hasher := md5.New()
+		hasher.Write([]byte(data.refer + data.ua))
+		uid := hex.EncodeToString(hasher.Sum(nil))
+
+		// 很多解析的工作都可放在这里完成
+		// ...
+		// ...
+		uData := urlData{
+			data,
+			uid,
+			formatUrl(data.url, data.time),
+		}
+
+		//log.Infoln(uData)
+
+		pvChannel <- uData
+		uvChannel <- uData
+		//xxChannel <- uData
+	}
+	return nil
+}
+func cutLogFetchData(logStr string) digData {
+	logStr = strings.TrimSpace(logStr)
+	pos1 := str.IndexOf(logStr, HANDLE_DIG, 0)
+	if pos1 == -1 {
+		return digData{}
+	}
+	pos1 += len(HANDLE_DIG)
+	pos2 := str.IndexOf(logStr, " HTTP/", pos1)
+	d := str.Substr(logStr, pos1, pos2-pos1)
+
+	urlInfo, err := url.Parse("http://localhost/?" + d)
+	if err != nil {
+		return digData{}
+	}
+	data := urlInfo.Query()
+	return digData{
+		data.Get("time"),
+		data.Get("refer"),
+		data.Get("url"),
+		data.Get("ua"),
+	}
 
 }
 
@@ -125,4 +251,45 @@ func readFileLineByLine(params cmdParams, logChannel chan string) error {
 		}
 	}
 	return nil
+}
+
+func formatUrl(url, t string) urlNode {
+	// 一定从量大的着手， 详情页>列表≥首页
+	pos1 := str.IndexOf(url, HANDLE_MOVIE, 0)
+	if pos1 != -1 {
+		pos1 += len(HANDLE_MOVIE)
+		pos2 := str.IndexOf(url, HANDLE_HTML, 0)
+		idStr := str.Substr(url, pos1, pos2-pos1)
+		id, _ := strconv.Atoi(idStr)
+		return urlNode{"movie", id, url, t}
+	} else {
+		pos1 = str.IndexOf(url, HANDLE_LIST, 0)
+		if pos1 != -1 {
+			pos1 += len(HANDLE_LIST)
+			pos2 := str.IndexOf(url, HANDLE_HTML, 0)
+			idStr := str.Substr(url, pos1, pos2-pos1)
+			id, _ := strconv.Atoi(idStr)
+			return urlNode{"list", id, url, t}
+		} else {
+			return urlNode{"home", 1, url, t}
+		} // 如果页面多种，不断在此扩展
+	}
+}
+
+func getTime(logTime, timeType string) string {
+	var item string
+	switch timeType {
+	case "day":
+		item = "2006-01-02"
+		//fallthrough   //必须是最后一个语句
+		break // 默认
+	case "hour":
+		item = "2006-01-02 15"
+		break
+	case "min":
+		item = "2006-01-02 15:04"
+		break
+	}
+	t, _ := time.Parse(item, time.Now().Format(item))
+	return strconv.FormatInt(t.Unix(), 10)
 }
